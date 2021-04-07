e6f3b5fa2e64ccc7f14f180dd2c17ae6a2ce7e3c
Kanika Sharma <kanica1606gmail.com>
2021-04-07 17:07:28 -0400

    UISlider and Second view controller

---
+++ BMI-Calculator-iOS/Controllers/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  BMI-Calculator-iOS
+//
+//  Created by Kanica Goddard on 2021-04-07.
+//
+
+import Foundation
+
+class SecondViewController: UIViewController {
+    
+}
